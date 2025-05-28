594acec457cfd9f3f9d36c948bd44d86d2d1ade9
murugan9658 <vraina266@gamil.com>
2025-05-28 21:02:12 +0530

    contact.jsx framer motion update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,57 +1,101 @@
 
 import { FaFacebook, FaInstagram ,FaTwitter} from "react-icons/fa";
+import {motion} from "framer-motion";
+
+
+
 
 const Contact = () => {
+
+
   return (
     <div className='w-full md:h-screen space-y-3 py-6 items-center justify-center bg-green-100 md:flex gap-5 text-black/60'>
       {/*contact content */}
         <div className='flex-1  px-10 space-y-5 '>
           <div className='space-y-6  '>
-            <h1 className='text-3xl font-playfair font-semibold capitalize'>
+            <motion.h1 
+              initial={{ x: -20, opacity: 0 }}
+              whileInView={{ x: 0, opacity: 1 }}
+              transition={{ delay: 0.2, type: "spring", stiffness: 100 }}
+              viewport={{ amount: 0.9, once: true }} 
+            className='text-3xl font-playfair font-semibold capitalize'>
               let's <span className='underline text-orange-500'>contact</span> with us
-            </h1>
-            <p className='text-lg  '>
+            </motion.h1>
+            <motion.p
+              initial={{ x: 20, opacity: 0 }}
+              whileInView={{ x: 0, opacity: 1 }}
+              transition={{ delay: 0.2, type: "spring", stiffness: 100 }}
+              viewport={{ amount: 0.6, once: true }}
+            className='text-lg  '>
                <span className='text-black text-lg'>Retro Real Estate</span>   is the best sit amet, consectetur adipiscing elitgula nunc a diam nunc laoreet penatibus mi odio. Dictum suspendisse semper curabitur libero, ultrices. Velit cursus ultricies lectus quam
-            </p>
+            </motion.p>
           </div>
 
           <div >
-            <form  className='flex flex-col w-full space-y-6'>
-              <input
+            <motion.form             
+              className='flex flex-col w-full space-y-6'>
+              <motion.input
                type="text"
                name="name" 
                placeholder='name'
-               className='p-3.5 rounded-lg border-2 bg-gray-100 placeholder:text-lg hover:scale-101 transition duration-300 placeholder:text-gray-600 placeholder:italic  focus:outline-none focus:border-blue-400' />
+              initial={{ x: -20, opacity: 0 }}
+              whileInView={{ x: 0, opacity: 1 }}
+              transition={{ delay: 0.2, type: "spring", stiffness: 100 }}
+              viewport={{ amount: 0.9, once: true }} 
+               className='p-3.5 rounded-lg border-2 bg-gray-100 placeholder:text-lg placeholder:text-gray-600 placeholder:italic  focus:outline-none focus:border-blue-400' />
 
-              <input
+              <motion.input
                type="email" 
                name="email"
                placeholder='email'
-               className='p-3.5 rounded-lg border-2 bg-gray-100 placeholder:text-lg hover:scale-101 transition duration-300 placeholder:text-gray-600 placeholder:italic  focus:outline-none focus:border-blue-400' />
+               initial={{ x: -20, opacity: 0 }}
+               whileInView={{ x: 0, opacity: 1 }}
+               transition={{ delay: 0.2, type: "spring", stiffness: 100 }}
+               viewport={{ amount: 0.9, once: true }} 
+               className='p-3.5 rounded-lg border-2 bg-gray-100 placeholder:text-lg placeholder:text-gray-600 placeholder:italic  focus:outline-none focus:border-blue-400' />
 
-              <textarea 
+              <motion.textarea 
               name="message"
               placeholder='message' 
-              className='p-3.5 rounded-lg border-2 h-30 bg-gray-100 placeholder:text-lg hover:scale-101 transition duration-300 placeholder:text-gray-600 placeholder:italic  focus:outline-none focus:border-blue-400' >
-              </textarea>
-              <button 
+              initial={{ x: -20, opacity: 0 }}
+              whileInView={{ x: 0, opacity: 1 }}
+              transition={{ delay: 0.2, type: "spring", stiffness: 100 }}
+              viewport={{ amount: 0.5, once: true }} 
+              className='p-3.5 rounded-lg border-2 h-30 bg-gray-100 placeholder:text-lg  placeholder:text-gray-600 placeholder:italic  focus:outline-none focus:border-blue-400' >
+              </motion.textarea >
+              <motion.button 
               type="submit"
+               initial={{ x: -20, opacity: 0 }}
+              whileInView={{ x: 0, opacity: 1 }}
+              transition={{ delay: 0.2, type: "spring", stiffness: 100 }}
+              viewport={{ amount: 0.9, once: true }} 
               className='py-1.5 px-3 w-40 hover:bg-blue-400 text-lg font-playfair transition duration-300 cursor-pointer hover:text-white hover:font-semibold border-2 rounded-lg '
-              >submit</button>
-            </form>
+              >submit</motion.button >
+            </motion.form>
           </div>
         </div>
 
         {/*contact img */}
 
         <div className='flex-1 space-y-6 px-10'>
-          <div className='overflow-hidden rounded-lg'>
-            <img src="/images/contactpic.jpg"
+          <motion.div
+            initial={{ x: 40, opacity: 0 }}
+            whileInView={{ x: 0, opacity: 1 }}
+            transition={{ delay: 0.2, duration: 0.6, ease: "easeOut" }}
+            viewport={{ amount: 0.2, once: true }} 
+            className='overflow-hidden rounded-lg'>
+            <img 
+             src="/images/contactpic.jpg"
              alt="contactpic"
              className='rounded-lg hover:scale-105 object-cover transition-transform duration-500'
              />
-          </div>
-          <div className='flex flex-col space-y-3'>
+          </motion.div>
+          <motion.div
+              initial={{ x: -20, opacity: 0 }}
+              whileInView={{ x: 0, opacity: 1 }}
+              transition={{ delay: 0.2, type: "spring", stiffness: 100 }}
+              viewport={{ amount: 0.2, once: true }} 
+             className='flex flex-col space-y-3'>
             <h1 className='text-3xl font-playfair text-black font-semibold'>
             Contact info
             </h1>
@@ -70,7 +114,7 @@ const Contact = () => {
                   <FaInstagram  className='hover:scale-105 transition duration-300' />
                   <FaTwitter  className='hover:scale-105 transition duration-300' />
                 </div>
-          </div>
+          </motion.div>
 
         </div>
     </div>
